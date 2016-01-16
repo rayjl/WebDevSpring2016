b1fdf7f6fdf478732c0f4d33d7d084632562fe2d
Raymond Li <rayjl@ccs.neu.edu>
2016-01-16 12:51:45 -0800

    Changes to server.js from Alex from class

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+#!/bin/env node
+//  OpenShift sample Node application
 var express = require('express');
 var app = express();
 
-
+//  Set the environment variables we need.
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var home      = 'This is a test for server.js'
 
-app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+    res.send(home)
+})
 
-app.listen(port, ipaddress);
+app.listen(port,ipaddress)
