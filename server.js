4fe4e11d62b47abe3fd2b8c1a0f86ad821c3b5c2
Raymond Li <rayjl@ccs.neu.edu>
2016-01-21 20:11:08 -0800

    Dev Environment - updating for assignment 0

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-#!/bin/env node
-//  OpenShift sample Node application
 var express = require('express');
 var app = express();
-
-//  Set the environment variables we need.
+app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var home      = 'This is a test for server.js'
-
-app.get('/', function (req, res) {
-    res.send(home)
-})
-
-app.listen(port,ipaddress)
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.get('/hello', function(req, res){
+    res.send('hello world');
+});
+app.listen(port, ipaddress);
