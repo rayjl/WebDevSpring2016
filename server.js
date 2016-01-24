f0783f4479fdb781f4093d47b03f55f7a54589a8
Raymond Li <rayjl@ccs.neu.edu>
2016-01-24 10:03:19 -0800

    Watching course videos and following the HTML practice - also added template

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+/**
+ * Server source code for assignment 1
+ * Copied over from the assignment 1 sheet
+ */
+
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/hello', function(req, res){
-    res.send('hello world');
-});
 app.listen(port, ipaddress);
