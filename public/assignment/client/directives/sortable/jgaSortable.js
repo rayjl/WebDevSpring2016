19b954613e12c1c938b6e5aeb1d0625cbfdb29fd
Raymond Li <rayjl@ccs.neu.edu>
2016-03-20 15:44:06 -0700

    added libraries to local and used professors directive on sorting

--- public/assignment/client/directives/sortable/jgaSortable.js
+++ public/assignment/client/directives/sortable/jgaSortable.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module("FormBuilderApp")
+        .module("jgaSortable", [])
         .directive("jgaSortable", jgaSortable);
 
     function jgaSortable() {
