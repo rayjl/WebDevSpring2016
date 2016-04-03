ca9c99b71ebe0a5355d5ab59c8fd1e75af0af6a4
Raymond Li <rayjl@ccs.neu.edu>
2016-04-03 09:26:38 -0700

    still got the bug

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -1,10 +1,11 @@
 'use strict';
 
 module.exports = function(app, mongoose) {
-    var userModel = require("./models/user.model.js")(mongoose);
-    var formModel = require("./models/form.model.js")(mongoose);
+    var userModel = require("./models/user.model.server.js")(mongoose);
+    var formModel = require("./models/form.model.server.js")(mongoose);
+    var fieldModel = require("./models/field.model.server.js")(mongoose);
 
     require("./services/user.service.server.js")(app, userModel);
     require("./services/form.service.server.js")(app, formModel);
-    require("./services/field.service.server.js")(app, formModel);
+    require("./services/field.service.server.js")(app, fieldModel);
 };
